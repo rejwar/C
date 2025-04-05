0d30dcddae45c288b4886ef33c354b17374d7b3f
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-05 21:52:23 +0600

    Create SimpleCalculator.c

---
+++ Basic/SimpleCalculator.c
@@ -0,0 +1,15 @@
+/*
+File Name: SimpleCalculator.c
+Purpose: Perform addition, subtraction, multiplication, and division.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a = 8, b = 4;
+    printf("Addition: %d\n", a + b);
+    printf("Subtraction: %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
+    printf("Division: %d\n", a / b);
+    return 0;
+}
