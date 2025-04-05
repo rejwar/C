b8c5d33bb188e7ef55fee7237ffecece5c6e37fd
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2025-04-05 21:58:55 +0600

    Create  SortArray.c

---
+++ Basic/SortArray.c
@@ -0,0 +1,25 @@
+/*
+File Name: SortArray.c
+Purpose: Sort an array in ascending order.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6}, n = 6, temp;
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
