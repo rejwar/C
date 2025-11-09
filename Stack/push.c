9d78183967920ba3741ac16b8404d28f9ca011b3
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-10 05:20:18 +0600

    new code

---
+++ Stack/push.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack overflow\n");
+    } else {
+        stack[++top] = value;
+    }
+}
+
+int pop() {
+    if (top == -1) {
+        printf("Stack underflow\n");
+        return -1;
+    } else {
+        return stack[top--];
+    }
+}
+
+int peek() {
+    if (top == -1)
+        return -1;
+    else
+        return stack[top];
+}
+
+int isEmpty() {
+    return top == -1;
+}
+
+void display() {
+    if (isEmpty())
+        printf("Stack is empty\n");
+    else {
+        printf("Stack: ");
+        for (int i = top; i >= 0; i--)
+            printf("%d ", stack[i]);
+        printf("\n");
+    }
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    display();
+    return 0;
+}
