0a9bbb705717ece8e206df96cee6f12eb5fee779
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-10 02:30:45 +0600

    New code

--- Stack/First.c
+++ Stack/First.c
@@ -7,8 +7,25 @@ void push(int x) {
     if (top < CAPACITY -1) {
         top = top +1;
         stack[top] = x;
-        printf("Success  added item ")
+        printf("Success  added item  %d" ,x);
     } else {
-        
+        printf("Exception! No spaces?n");
     }
 } 
+
+int pop() {
+    return -1;
+}
+
+int peek() {
+    return -1;
+}
+
+int main() {
+    printf(" Implementing my stack in C.\n");
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    return 0;
+}
