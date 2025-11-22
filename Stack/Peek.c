139d3cebac4abc5f518da082269e20c7f35995dd
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-11-22 07:16:31 +0600

    new code

---
+++ Stack/Peek.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+int isEmpty() {
+    return top == -1;
+}
+
+int peek() {
+    if (isEmpty()) {
+        printf("STACK Empty \n");
+        return -1;
+    }
+    return stack[top];
+}
+
+
+int main() {
+    printf("Peek resutlt = %d\n" , peek());
+
+    top = 1;
+    stack[0] = 10;
+    stack[1] = 20;
+}
